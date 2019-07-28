1e17358aa4d924b9d4db4f657e877fcdada4a773
mahdi khanzadi <khanzadimahdi@gmail.com>
2019-07-28 23:16:38 +0430

    add Tsms gateway

---
+++ src/Drivers/Tsms.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Tzsk\Sms\Drivers;
+
+use Tzsk\Sms\Abstracts\Driver;
+use \SoapClient;
+
+class Tsms extends Driver
+{
+    /**
+     * Tsms Settings.
+     *
+     * @var null|object
+     */
+    protected $settings;
+
+    /**
+     * Smsir Client.
+     *
+     * @var null|SoapClient
+     */
+    protected $client;
+
+    /**
+     * Construct the class with the relevant settings.
+     *
+     * @param $settings
+     * @throws \SoapFault
+     */
+    public function __construct($settings)
+    {
+        $this->settings = (object) $settings;
+        $this->client = new SoapClient($this->settings->url);
+    }
+
+    /**
+     * Send text message and return response.
+     *
+     * @return object
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function send()
+    {
+        $response = collect();
+        foreach ($this->recipients as $recipient) {
+            $result = $this
+                ->client
+                ->sendSms(
+                    $this->settings->username,
+                    $this->settings->password,
+                    [$this->settings->from],
+                    [$recipient],
+                    [$this->body],
+                    [],
+                    rand()
+                );
+
+            $response->put($recipient, $result);
+        }
+
+        return (count($this->recipients) == 1) ? $response->first() : $response;
+    }
+}
