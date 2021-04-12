06cb5fe470475545b3ad61a545f29992021d4639
sepisoltani <sph_1996@yahoo.com>
2021-04-13 00:02:01 +0430

    replace cURL with GuzzleHttp

--- src/Drivers/Ghasedak.php
+++ src/Drivers/Ghasedak.php
@@ -1,42 +1,43 @@
 <?php
 
-
 namespace Tzsk\Sms\Drivers;
 
-
+use GuzzleHttp\Client;
 use Tzsk\Sms\Contracts\Driver;
 
 class Ghasedak extends Driver
 {
     protected array $settings;
 
-    protected $client;
+    protected Client $client;
 
     public function __construct(array $settings)
     {
         $this->settings = $settings;
-        $this->client = curl_init();
+        $this->client = new Client();
     }
 
     public function send()
     {
-        $sender = $this->settings['from'];
         $response = collect();
         foreach ($this->recipients as $recipient) {
-            curl_setopt_array($this->client, array(
-                CURLOPT_URL            => $this->settings['url'] . '/v2/sms/send/simple',
-                CURLOPT_HTTP_VERSION   => CURL_HTTP_VERSION_1_1,
-                CURLOPT_CUSTOMREQUEST  => "POST",
-                CURLOPT_RETURNTRANSFER => true,
-                CURLOPT_POSTFIELDS     => "message=$this->body&sender=$sender&Receptor=$recipient",
-                CURLOPT_HTTPHEADER     => array(
-                    "apikey: " . $this->settings['apiKey'],
-                ),
-            ));
-            $result = curl_exec($this->client);
+            $result = $this->client->request(
+                'POST',
+                $this->settings['url'] . '/v2/sms/send/simple',
+                [
+                    'form_params' => [
+                        'Receptor' => $recipient,
+                        'sender' => $this->settings['from'],
+                        'message' => $this->body,
+                    ],
+                    'headers' => [
+                        'apikey' => $this->settings['apiKey'],
+                    ],
+                ]
+            );
             $response->put($recipient, $result);
         }
-        curl_close($this->client);
+
         return (count($this->recipients) == 1) ? $response->first() : $response;
     }
 }
